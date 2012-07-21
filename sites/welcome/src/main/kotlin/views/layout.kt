8d9c16f4b86e6bb0e0a2e6f0b5e7457b66753681
Andrea Parodi <parroit@ebansoft.com>
2012-07-21 09:24:12 +0200

    ported layout to kool templates

--- sites/welcome/src/main/kotlin/views/layout.kt
+++ sites/welcome/src/main/kotlin/views/layout.kt
@@ -1,5 +1,66 @@
 package templates
 import kw.views.*
-/*this is just a stub that get replaced
-on app start by compiled layout.html.kt*/
-fun layout(content:() -> String) = htmlView { "" }
+import io.kool.template.html.*
+fun layout(content:() -> String) = view {
+    html{
+       head{
+        title("Kweb Kool template sample:Quotes list")
+        this + importJavascript("jquery-1.7.1.min")
+        this + importJavascript("bootstrap-alert")
+        this + importJavascript("bootstrap-modal")
+        this + importLess("bootstrap")
+
+       }
+       body{
+            div(klass="container"){
+                div (id="header", klass="row"){
+                    div (klass="span12")  {
+                        div (klass="navbar navbar-fixed-top"){
+							div (klass="navbar-inner"){
+								div (klass="container"){
+
+									img(klass="brand", src='/public/images/ammi.png')
+									span(klass="name brand"){
+										+"Kweb sample:quotes/span"
+									}
+
+									ul (klass="nav"){
+									li (klass="active"){
+                                        a (href="#",text="Home/a")
+
+                                        li {a( href="#",text="Bills")}
+                                        li {a( href="#",text= Company)}
+
+                                        li {a( href=App.quotes_list_page0(), text= "Quotes")}
+									
+								}
+
+							}
+
+						}
+					}
+				}
+
+
+				div (id="content", klass="row"){
+					div (klass="span12"){
+						this + content()
+					}
+
+				}
+
+				div (id="footer"){
+
+					div (klass="navbar navbar-fixed-bottom"){
+						div (klass="navbar-inner"){
+							div (klass="container"){
+								+ "A wonderful world"
+							}
+						}
+					}
+
+				}
+			}
+		}
+	}.toString()!!
+}
